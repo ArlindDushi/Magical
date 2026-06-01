1368780baf797195d8ecf7f517202f4690e10d38
Arlind Dushi <dushi.arlind@gmail.com>
2026-06-01 17:17:25 +0200

    New version 1.1.1

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
     targets: [
         .binaryTarget(
             name: "Magical",
-            url: "https://github.com/ArlindDushi/Magical/releases/download/1.1.0/Magical.xcframework.zip",
+            url: "https://github.com/ArlindDushi/Magical/releases/download/1.1.1/Magical.xcframework.zip",
             checksum: "a4fd346e7e7a997fdde664c08c78882d7d1d46a27bb2ebae40b13db330d04251"
         )
     ]
